aa5f2b150bc8dbc95ff724081bd7ca6bab360e1a
Mahbub <mahbub5015@gmail.com>
2023-11-11 20:32:38 +0600

    doctor portal client added

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { createBrowserRouter } from 'react-router-dom';
+import Main from '../Layout/Main';
+import Home from '../Pages/Home/Home';
+import About from '../Pages/About/About';
+
+
+const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Main></Main>,
+      children: [
+        {
+            path: '/',
+            element:<Home></Home>
+        },
+        {
+            path: '/about',
+            element:<About></About>
+        }
+      ]
+    },
+  ]);
+  
+
+export default router;
